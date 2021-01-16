db82ba2fe07cf48ea4194146d5e1cb476e2fb94c
shakil51298 <zikaiwen02@gmail.com>
2021-01-17 03:56:20 +0800

    loop.js

---
+++ loop.js
@@ -0,0 +1,19 @@
+// //                      LOOP
+// // Wile Loop
+// var realNumber = 0;
+// while(realNumber <=15 ){
+//     console.log(realNumber);
+//     realNumber++;
+// }
+
+// FOR Loop
+
+for(var i = 0; i <=15; i++){
+    console.log(i);
+}
+// ami chassi sting gula k output korty
+var shakil = ('shakils', 'szad', 'kobir', 'rahuim');
+for(i=0;i<shakil.length;i++);{
+    var names = shakil(0);
+    console.log(names);
+}
